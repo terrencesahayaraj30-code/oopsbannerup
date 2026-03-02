bacaa42c1a68c7d2f424c908006bbda23d9ffa9a
TERRENCE <terrencesahayaraj30@gmail.com>
2026-03-02 14:17:32 +0530

    Update OOPSBannerUC3.java

--- OOPSBannerUC3.java
+++ OOPSBannerUC3.java
@@ -1,36 +1,10 @@
-public class OOPSBannerUC3 {
+public class OOPSBannerUC3{
+    public static void main(String[] args) { 
 
-    public static void main(String[] args) {
-        printO();
-        printO();
-        printP();
-        printS();
-    }
-
-    public static void printO() {
-        System.out.println(" ***** ");
-        System.out.println(" *   * ");
-        System.out.println(" *   * ");
-        System.out.println(" *   * ");
-        System.out.println(" ***** ");
-        System.out.println();
-    }
-
-    public static void printP() {
-        System.out.println(" ***** ");
-        System.out.println(" *   * ");
-        System.out.println(" ***** ");
-        System.out.println(" *     ");
-        System.out.println(" *     ");
-        System.out.println();
-    }
-
-    public static void printS() {
-        System.out.println(" ***** ");
-        System.out.println(" *     ");
-        System.out.println(" ***** ");
-        System.out.println("     * ");
-        System.out.println(" ***** ");
-        System.out.println();
+ System.out.println(String.join(" ","  ***  "," ***** "," ***** "," ***** "));
+ System.out.println(String.join(" "," *   * "," *   * "," *   * "," *     "));
+ System.out.println(String.join(" "," *   * "," *   * "," ***** "," ***** "));
+ System.out.println(String.join(" "," *   * "," *   * "," *     ","     * "));
+ System.out.println(String.join(" ","  ***  "," ***** "," *     "," ***** "));
     }
 }
