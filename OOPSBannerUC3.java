93d91701b065e7826861f9eb5f6ded4407cb7d68
TERRENCE <terrencesahayaraj30@gmail.com>
2026-03-02 13:19:52 +0530

    Add files via upload

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,36 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+        printO();
+        printO();
+        printP();
+        printS();
+    }
+
+    public static void printO() {
+        System.out.println(" ***** ");
+        System.out.println(" *   * ");
+        System.out.println(" *   * ");
+        System.out.println(" *   * ");
+        System.out.println(" ***** ");
+        System.out.println();
+    }
+
+    public static void printP() {
+        System.out.println(" ***** ");
+        System.out.println(" *   * ");
+        System.out.println(" ***** ");
+        System.out.println(" *     ");
+        System.out.println(" *     ");
+        System.out.println();
+    }
+
+    public static void printS() {
+        System.out.println(" ***** ");
+        System.out.println(" *     ");
+        System.out.println(" ***** ");
+        System.out.println("     * ");
+        System.out.println(" ***** ");
+        System.out.println();
+    }
+}
